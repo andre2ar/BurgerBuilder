2ed42918c868bb322472c2dfde63e7985e19a347
André <andre2ar@outlook.com>
2019-10-31 11:30:56 -0300

    Burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
-    const ingredients = Object.keys(props.ingredients).map(ingredient => {
+    let ingredients = Object.keys(props.ingredients).map(ingredient => {
         return [...Array(props.ingredients[ingredient])].map((_, i) => {
             return <BurgerIngredient type={ingredient} key={ingredient + i}/>;
         });
-    });
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    if (ingredients.length === 0){
+        ingredients = <p>Please start adding ingredients</p>
+    }
 
     return (
         <div className={classes.Burger}>
