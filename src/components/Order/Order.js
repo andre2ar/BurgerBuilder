befb944f42fac61a7f78ca2f368889b0bca7708e
André <andre2ar@outlook.com>
2019-11-13 14:52:41 -0300

    Orders

---
+++ src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Order.module.css';
+
+const Order = (props) => (
+    <div className={classes.Order}>
+        <p>Ingredients: 5</p>
+    </div>
+);
+
+export default Order;
