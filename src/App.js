3e87e33d2a50c5a1e265a599ec845dd0e9262ba0
André <andre2ar@outlook.com>
2019-12-03 09:48:33 -0300

    Auth

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import Layout from './containers/Layout/Layout';
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
+import Auth from "./containers/Auth/Auth";
 
 function App() {
   return (
     <div>
       <Layout>
           <Switch>
-              <Route path='/checkout' component={Checkout} />
-              <Route path='/orders' component={Orders } />
-              <Route path='/' exact component={BurgerBuilder} />
+              <Route path='/checkout' component={ Checkout } />
+              <Route path='/orders' component={ Orders } />
+              <Route path='/auth' component={ Auth } />
+              <Route path='/' exact component={ BurgerBuilder } />
           </Switch>
       </Layout>
     </div>
